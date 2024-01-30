ff78ef5010c93e1f86c5577c6b5b351c1b4d8d56
rushil <rushilvegada@gmail.com>
2024-01-30 10:34:31 -0500

    learn.py changes

---
+++ src/hw3/learn.py
@@ -0,0 +1,18 @@
+from data import Data
+
+def learn(data, row, my, the):
+    my['n'] += 1
+    kl = row.cells[data.cols.klass.at]
+    
+    # print("Once", my['datas'])
+    # print(kl)
+    
+    if my['n'] > 10:
+        my['tries'] += 1
+        my['acc'] += 1 if kl == row.likes(my['datas'])[0] else 0
+    
+    if not my['datas']:
+        my['datas'] = {}
+    
+    my['datas'][kl] = my['datas'].get(kl, Data(the, [data.cols.names]))
+    my['datas'][kl].add(row)
