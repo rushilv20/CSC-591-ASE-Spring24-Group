3b0b5b171a40e1535b63a7f69c9bb48ff13bd8b8
Swarangi Gaurkar <49075539+Swarangigaurkar@users.noreply.github.com>
2024-02-20 09:04:22 -0500

    Update util.py

--- src/hw4/util.py
+++ src/hw4/util.py
@@ -20,6 +20,46 @@ class Utility:
         try:
             return ast.literal_eval(s)
         except Exception: return s.strip() #when s is already a string
+            
+def do_file(file):
+    # if local X = y is present, find both the thing to replace and what to replace it with
+    data = None
+    with open(file, "r") as fp:
+        data = fp.read()
+    vars = re.match("local (.*) = (.*)\n", data)
+    if vars:
+        variable, value = vars.groups()
+        data = data.replace(variable, value)
+        data = re.sub("local .* = .*\n", "", data)
+    # remove the return statement
+    data = data.replace("return ", "")
+    # remove newlines
+    data = data.replace("\n", "")
+    # replace domain= , cols= , rows=
+    # change X=y to "X":y
+    terms = ["domain", "cols", "rows"]
+    for term in terms:
+        data = re.sub("{}\s*=".format(term), '"{}":'.format(term), data)
+    # replace { } with [ ]
+    first, last = data.index("{"), data.rindex("}")
+    data = data[first + 1:last].replace("{", "[").replace("}", "]")
+    data = "{" + data + "}"
+
+    # replace ' with "
+    data = data.replace("'", '"')
+    json_obj = json.loads(data)
+    return json_obj
+
+
+def oo(t):
+    td = t.__dict__
+    td['a'] = t.__class__.__name__
+    td['id'] = id(t)
+    print(dict(sorted(td.items())))
+
+
+def last(t):
+    return t[-1]
 
 
     def round(n, ndecs=None):
